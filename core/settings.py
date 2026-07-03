03c79a23a50689ff5552ae2241027b646ca2b7ce
Maqsudbek <dehkanaliev@gmail.com>
2026-07-03 21:29:48 +0500

    Settingdagi sozlamalar to'girlandi

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'users',
+    'resumes',
+    'jobs',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'users.CustomUser'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    STATUS = (
+        ('worker', 'Worker'),
+        ('employer', 'Employer')
+    )
+    
+    role = models.CharField(max_length=10, choices=STATUS)
+    phone = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"

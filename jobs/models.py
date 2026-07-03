7a5de9f209ae8c6e2803941d3aeb7c08fa506a51
Maqsudbek <dehkanaliev@gmail.com>
2026-07-03 22:24:37 +0500

    Job va Company modellarini yaratdim va migratsiya qildim!

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Company(models.Model):
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    website = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.owner.first_name} | {self.name}"
+    
+    
+
+class Job(models.Model):
+    JOBTYPES = (
+        ('full-time', 'Full-time'),
+        ('part-time', 'Part-time'),
+        ('remote', 'Remote'),
+        ('internship', 'Internship'),
+    )
+    
+    employer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    category = models.CharField(max_length=30)
+    location = models.CharField(max_length=150)
+    salary_min = models.DecimalField(max_digits=20, decimal_places=2)
+    salary_max = models.DecimalField(max_digits=20, decimal_places=2)
+    job_type = models.CharField(max_length=20, choices=JOBTYPES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class CustomUser(AbstractUser):
     
     role = models.CharField(max_length=10, choices=STATUS)
     phone = models.CharField(max_length=20)
+    profile_img = models.ImageField(upload_to='avtrs/', blank=True, default='avtrs/default_avtr.jpg')
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"

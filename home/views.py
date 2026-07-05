bfcd13dc855bfe4248536f19fb7157269116eb1b
Maqsudbek <dehkanaliev@gmail.com>
2026-07-06 00:50:35 +0500

    I have created the emp-base

--- home/views.py
+++ home/views.py
@@ -3,4 +3,6 @@ from django.views import View
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home.html')
+        if request.user.role == 'employer':
+            base_template = 'emp-base.html'
+        return render(request, 'home.html', {'base_template': base_template})
